fb6646f37d6508b5ed0d1cebd4312e75f46b2684
LEE YEJIN <dldulwls@gmail.com>
2024-02-07 16:05:25 +0900

    게임시작, 리셋버튼 구현

--- baseball.js
+++ baseball.js
@@ -1,7 +1,11 @@
+let gameStartBtn = document.getElementById("gameStartBtn");
+let gameContainer = document.getElementById("gameContainer");
 let answerBtn = document.getElementById("answerBtn");
 let userAns = document.getElementById("userAnswer");
 let gameArea = document.getElementById("gameArea");
+let userHistory_content = document.getElementById("userHistory_content");
 let answerArea = document.getElementById("answerArea");
+let resetBtn = document.getElementById("resetBtn");
 let userAnswer = "";
 let randomNum = [];
 let ball;
@@ -17,17 +21,18 @@ answerBtn.addEventListener("click", () => {
   cNum(randomStr);
   console.log(`${ball}B${strike}S`);
   count++;
-  gemeover(strike)
-})
+  gemeover(strike);
+  // text 뜨게
+  message();
+});
 
 gemeover = (strike) => {
   console.log(strike);
-  if(strike === 3){
-    gameArea.style.display="none";
-    answerArea.innerHTML=`${count}번 만에 성공하셨습니다.`
+  if (strike === 3) {
+    gameArea.style.display = "none";
+    answerArea.innerHTML = `${count}번 만에 성공하셨습니다.`;
   }
-
-}
+};
 
 // 랜덤 숫자 반환
 rNumber = () => {
@@ -40,7 +45,7 @@ rNumber = () => {
   }
   console.log(randomNum.join(""));
   return randomNum.join("");
-}
+};
 
 // 숫자 비교 카운팅
 cNum = (str) => {
@@ -51,9 +56,29 @@ cNum = (str) => {
       ball++;
     }
   }
-  // 3. 볼, 스트라이크를 표현하는 부분 구현하기
-  
-}
-
-
+};
+// 3. 볼, 스트라이크를 표현하는 부분 구현하기
+message = () => {
+  let content = `<ul>
+  <li>${count}번째 시도 : ${userAnswer}</li>
+  <li>${ball}B${strike}S</li>
+  </ul>`;
+  console.log(ball, strike);
+  userHistory_content.innerHTML += content;
+  userAns.value = "";
+};
 // 4. 게임 종료하는 부분 구현하기
+
+// 게임 시작 버튼
+function gameDisplay() {
+  gameContainer.classList.remove("hidden");
+  gameStartBtn.classList.add("hidden");
+}
+// 리셋 버튼
+function reset() {
+  //텍스트 다 지우기
+  count = 0;
+  userHistory_content.innerHTML = "";
+  //랜덤숫자 다시 세팅
+  randomNum = [];
+}
